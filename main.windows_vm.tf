2702faa55df85a60c28a6b88f08ee4837f715a7f
James Wilkinson <JWilkinson@mayerbrown.com>
2025-11-21 12:17:13 +0000

    add ignore_changes

--- main.windows_vm.tf
+++ main.windows_vm.tf
@@ -178,7 +178,7 @@ resource "azurerm_windows_virtual_machine" "this" {
       admin_password,
       source_image_reference,
       source_image_id,
-      os_disk.name,
+      os_disk[0].name,
     ]
   }
 }
